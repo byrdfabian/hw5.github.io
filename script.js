4ab870d5028602fbc32c06f74b5cd7b2e2ca4cbe
Fabian Byrd <fabianbyrd@gmail.com>
2020-08-19 14:42:49 -0400

    homework

---
+++ script.js
@@ -0,0 +1,8 @@
+var d = new Date();
+d.setFullYear(2020);
+document.getElementById("currentDay").innerHTML = d;
+
+function myFunction() {
+    document.getElementById("myTextarea").cols.length = "2";
+}
+
